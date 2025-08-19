d5486b17ca357d3011f5bbaa5ecb50da2c74b766
Mohamed sr <sermos73@gmail.com>
2025-08-19 01:29:44 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyA_b4KuDnM4530t1uDj2NaZdhr3d-QGnPc",
+  authDomain: "rentify-n8ge3.firebaseapp.com",
+  databaseURL: "https://rentify-n8ge3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rentify-n8ge3",
+  storageBucket: "rentify-n8ge3.firebasestorage.app",
+  messagingSenderId: "1025540698370",
+  appId: "1:1025540698370:web:3b1c49da2b4dd1fe87d21b"
 };
 
 // Initialize Firebase
