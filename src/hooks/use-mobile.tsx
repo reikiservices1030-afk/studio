ec9ee13ff355ea342b577aa217b885651d3a57f6
Mohamed sr <sermos73@gmail.com>
2025-08-20 00:08:18 +0000

    Hydration failed because the server rendered HTML didn't match the clien

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,15 +3,14 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
-    // This function now safely checks for the window object.
     const checkDevice = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
     
-    // Initial check on the client-side after mount.
+    // Initial check
     checkDevice();
 
     // Add resize listener
